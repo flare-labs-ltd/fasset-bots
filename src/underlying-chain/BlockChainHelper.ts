a999f80e39ed72ab4faa127fd823d8a89a84594e
Urska Krivc <urska.krivc>
2022-09-21 14:13:56 +0200

    add wrapper for IBlockChain, small changes in wallet wrapper

---
+++ src/underlying-chain/BlockChainHelper.ts
@@ -0,0 +1,73 @@
+import { MCC, MccClient } from "@flarenetwork/mcc";
+import { IBlock, IBlockChain, IBlockId, ITransaction, TxInputOutput } from "./interfaces/IBlockChain";
+import { WalletClient } from "simple-wallet/src/types";
+import { toBN } from "../utils/helpers";
+
+export class BlockChainHelper implements IBlockChain {
+    constructor(
+        public walletClient: WalletClient,
+        public mccClient: MccClient,
+    ) {}
+
+    finalizationBlocks: number = 0;
+    secondsPerBlock: number = 0;
+
+    async getTransaction(txHash: string): Promise<ITransaction | null> {
+        const transaction = await this.mccClient.getTransaction(txHash);
+        if (transaction) {
+            const inputs: TxInputOutput[] = [];
+            const outputs: TxInputOutput[] = [];
+            for (let input of transaction.spentAmounts) {
+                inputs.push([input.address ? input.address : "", input.amount]);
+            }
+            for (let output of transaction.spentAmounts) {
+                outputs.push([output.address ? output.address : "", output.amount]);
+            }
+            return {
+                hash: transaction.hash,
+                inputs: inputs,
+                outputs: outputs,
+                reference: transaction.stdPaymentReference,
+                status: transaction.successStatus
+            };
+        }
+        return null;
+    }
+
+    async getTransactionBlock(txHash: string): Promise<IBlockId | null> {
+        throw new Error("Method not implemented.");
+    }
+
+    async getBalance(address: string): Promise<import("bn.js")> {
+        const balance = await this.walletClient.getAccountBalance(address);
+        return toBN(balance);
+    }
+
+    async getBlock(blockHash: string): Promise<IBlock | null> {
+        if (this.mccClient instanceof MCC.ALGO) {
+            throw new Error("Method not implemented in ALGO.");
+        } else {
+            const block = await this.mccClient.getBlock(blockHash);
+            return {
+                hash: block.blockHash,
+                number: block.number,
+                timestamp: block.unixTimestamp,
+                transactions: block.transactionIds
+            };
+        }
+    }
+
+    async getBlockAt(blockNumber: number): Promise<IBlock | null> {
+        const block = await this.mccClient.getBlock(blockNumber);
+        return {
+            hash: block.blockHash,
+            number: block.number,
+            timestamp: block.unixTimestamp,
+            transactions: block.transactionIds
+        };
+    }
+
+    async getBlockHeight(): Promise<number> {
+        return await this.mccClient.getBlockHeight();
+    }
+}
--- src/underlying-chain/BlockChainWalletHelper.ts
+++ src/underlying-chain/BlockChainWalletHelper.ts
@@ -1,18 +1,15 @@
-import { WALLET } from "simple-wallet/src";
 import { PersistenceContext } from "../config/PersistenceContext";
-import { SourceId } from "../verification/sources/sources";
 import { IBlockChain } from "./interfaces/IBlockChain";
 import { IBlockChainWallet, TransactionOptions, TransactionOptionsWithFee } from "./interfaces/IBlockChainWallet";
 import { DBWalletKeys } from "./WalletKeys";
+import { WalletClient } from "simple-wallet/src/types";
 
-export class WalletHelper implements IBlockChainWallet {
+export class BlockChainWalletHelper implements IBlockChainWallet {
     constructor(
-        public chain: IBlockChain,
-        public chainId: SourceId,
-        public wClient: any, // should be defined as WALLET.<chain> aka WALLET.BTC = new WALLET.BTC(connection);
-        private pc: PersistenceContext
-    ) {
-    }
+        public walletClient: WalletClient,
+        private pc: PersistenceContext,
+        public chain: IBlockChain
+    ) {}
 
     async addTransaction(sourceAddress: string, targetAddress: string, amount: string | number | import("bn.js"), reference: string | null, options?: TransactionOptionsWithFee): Promise<string> {
         const walletKeys = new DBWalletKeys(this.pc);
@@ -20,11 +17,15 @@ export class WalletHelper implements IBlockChainWallet {
         //TODO add custom fee
         const fee = undefined;
         const note = reference ? reference : undefined;
-        const tr = await this.wClient.preparePaymentTransaction(sourceAddress, targetAddress, value, fee, note)
+        const tr = await this.walletClient.preparePaymentTransaction(sourceAddress, targetAddress, value, fee, note);
         const privateKey = await walletKeys.getKey(sourceAddress);
-        const txSigned = await this.wClient.signTransaction(tr, privateKey);
-        const submit = await this.wClient.submitTransaction(txSigned);
-        return submit.txId;
+        if (privateKey) {
+            const txSigned = await this.walletClient.signTransaction(tr, privateKey);
+            const submit = await this.walletClient.submitTransaction(txSigned);
+            return submit.txId;
+        } else {
+            throw new Error(`Cannot find address ${sourceAddress}`);
+        }
     }
 
     addMultiTransaction(spend: { [address: string]: string | number | import("bn.js"); }, receive: { [address: string]: string | number | import("bn.js"); }, reference: string | null, options?: TransactionOptions): Promise<string> {
@@ -33,7 +34,7 @@ export class WalletHelper implements IBlockChainWallet {
 
     async createAccount(): Promise<string> {
         const walletKeys = new DBWalletKeys(this.pc);
-        const account = await this.wClient.createWallet();
+        const account = await this.walletClient.createWallet();
         await walletKeys.addKey(account.address, account.privateKey);
         return account.address;
     }
