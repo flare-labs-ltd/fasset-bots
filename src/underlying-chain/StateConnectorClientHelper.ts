f2ababe66cb8b18206344e1a3b0b86057f945d89
Iztok <iztok@abelium.com>
2022-10-11 17:42:03 +0200

    state connector helper error checks

--- src/underlying-chain/StateConnectorClientHelper.ts
+++ src/underlying-chain/StateConnectorClientHelper.ts
@@ -1,15 +1,21 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import { AttestationClientSCInstance, IStateConnectorInstance } from "../../typechain-truffle";
-import { MerkleTree } from "../utils/MerkleTree";
 import { requiredEventArgs } from "../utils/events/truffle";
-import { sleep, toBN, toNumber } from "../utils/helpers";
-import { hexlifyBN } from "../verification/attestation-types/attestation-types-helpers";
-import { DHType } from "../verification/generated/attestation-hash-types";
+import { BNish, sleep, toBN, toNumber } from "../utils/helpers";
+import { MerkleTree } from "../utils/MerkleTree";
+import { web3DeepNormalize } from "../utils/web3normalize";
+import { DHBalanceDecreasingTransaction, DHConfirmedBlockHeightExists, DHPayment, DHReferencedPaymentNonexistence, DHType } from "../verification/generated/attestation-hash-types";
 import { encodeRequest } from "../verification/generated/attestation-request-encode";
 import { AttestationType } from "../verification/generated/attestation-types-enum";
 import { AttestationRequest, AttestationResponse, IStateConnectorClient } from "./interfaces/IStateConnectorClient";
 
 const DEFAULT_TIMEOUT = 15000;
+
+export class StateConnectorError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
 
 export class StateConnectorClientHelper implements IStateConnectorClient {
 
@@ -17,6 +23,7 @@ export class StateConnectorClientHelper implements IStateConnectorClient {
 
     // initialized at initStateConnector()
     stateConnector!: IStateConnectorInstance;
+    attestationClient!: AttestationClientSCInstance;
     firstEpochStartTime!: number;
     roundDurationSec!: number;
 
@@ -42,10 +49,12 @@ export class StateConnectorClientHelper implements IStateConnectorClient {
     async initStateConnector() {
         const IStateConnector = this.artifacts.require("IStateConnector");
         this.stateConnector = await IStateConnector.at(this.stateConnectorAddress);
+        const AttestationClientSC = this.artifacts.require("AttestationClientSC");
+        this.attestationClient = await AttestationClientSC.at(this.attestationClientAddress);
         this.firstEpochStartTime = toNumber(await this.stateConnector.BUFFER_TIMESTAMP_OFFSET());
         this.roundDurationSec = toNumber(await this.stateConnector.BUFFER_WINDOW());
     }
-    
+
     static async create(artifacts: Truffle.Artifacts, url: string, attestationClientAddress: string, stateConnectorAddress: string, account: string) {
         const helper = new StateConnectorClientHelper(artifacts, url, attestationClientAddress, stateConnectorAddress, account);
         await helper.initStateConnector();
@@ -66,7 +75,7 @@ export class StateConnectorClientHelper implements IStateConnectorClient {
 
     async waitForRoundFinalization(round: number): Promise<void> {
         let roundFinalized = false;
-        while (!roundFinalized) { 
+        while (!roundFinalized) {
             roundFinalized = await this.roundFinalized(round);
             await sleep(5000);
         }
@@ -88,14 +97,17 @@ export class StateConnectorClientHelper implements IStateConnectorClient {
     }
 
     async obtainProof(round: number, requestData: string): Promise<AttestationResponse<DHType>> {
-        const AttestationClientSC = this.artifacts.require("AttestationClientSC");
-        const attestationClient: AttestationClientSCInstance = await AttestationClientSC.at(this.attestationClientAddress);
+        try {
+            const resp = await this.client.get(`/api/proof/votes-for-round/${round}`);
+            const status = resp.data.status;
+            const data = resp.data.data;
 
-        const resp = await this.client.get(`/api/proof/votes-for-round/${round}`);
-        const status = resp.data.status;
-        const data = resp.data.data;
+            // is the round finalized?
+            if (status !== "OK") {
+                return { finalized: false, result: null };
+            }
 
-        if (status === "OK" && data.length > 0) {
+            // find response matching requestData
             let matchedResponse: any = null;
             for (let item of data) {
                 let encoded = encodeRequest(item.request);
@@ -103,118 +115,140 @@ export class StateConnectorClientHelper implements IStateConnectorClient {
                     matchedResponse = item;
                 }
             }
-            if (matchedResponse) {
-                let hashes: string[] = data.map((item: any) => item.hash) as string[];
-                const tree = new MerkleTree(hashes);
-                const index = tree.sortedHashes.findIndex((hash) => hash === matchedResponse.hash);
-                const proof = tree.getProof(index);
-
-                let blockchainVerification = false;
-                try {
-                    let responseHex = hexlifyBN(matchedResponse.response);
-                    responseHex.merkleProof = proof;
-                    responseHex.stateConnectorRound = round + 2;
-                    const sourceId = matchedResponse.request.sourceId;
-
-                    switch (matchedResponse.request.attestationType) {
-                        case AttestationType.Payment:
-                            console.log('Payment');
-                            blockchainVerification = await attestationClient.verifyPayment(sourceId, responseHex);
-                            if (blockchainVerification) {
-                                return {
-                                    finalized: blockchainVerification,
-                                    result: {
-                                        stateConnectorRound: matchedResponse.stateConnectorRound,
-                                        merkleProof: proof ? proof : undefined,
-                                        blockNumber: toBN(matchedResponse.blockNumber),
-                                        blockTimestamp: toBN(matchedResponse.blockTimestamp),
-                                        transactionHash: matchedResponse.transactionHash,
-                                        inUtxo: toBN(matchedResponse.inUtxo),
-                                        utxo: toBN(matchedResponse.utxo),
-                                        sourceAddressHash: matchedResponse.sourceAddressHash,
-                                        receivingAddressHash: matchedResponse.receivingAddressHash,
-                                        spentAmount: toBN(matchedResponse.spentAmount),
-                                        receivedAmount: toBN(matchedResponse.receivedAmount),
-                                        paymentReference: matchedResponse.paymentReference,
-                                        oneToOne: matchedResponse.oneToOne,
-                                        status: toBN(status)
-                                    }
-                                }
-                            }
-                            break;
-                        case AttestationType.BalanceDecreasingTransaction:
-                            console.log('verifyBalanceDecreasingTransaction');
-                            blockchainVerification = await attestationClient.verifyBalanceDecreasingTransaction(sourceId, responseHex);
-                            if (blockchainVerification) {
-                                return {
-                                    finalized: blockchainVerification,
-                                    result: {
-                                        stateConnectorRound: matchedResponse.stateConnectorRound,
-                                        merkleProof: proof ? proof : undefined,
-                                        blockNumber: toBN(matchedResponse.blockNumber),
-                                        blockTimestamp: toBN(matchedResponse.blockTimestamp),
-                                        transactionHash: matchedResponse.transactionHash,
-                                        inUtxo: toBN(matchedResponse.inUtxo),
-                                        sourceAddressHash: matchedResponse.sourceAddressHash,
-                                        spentAmount: toBN(matchedResponse.spentAmount),
-                                        paymentReference: matchedResponse.paymentReference
-                                    }
-                                }
-                            }
-                            break;
-                        case AttestationType.ConfirmedBlockHeightExists:
-                            console.log('verifyConfirmedBlockHeightExists');
-                            blockchainVerification = await attestationClient.verifyConfirmedBlockHeightExists(sourceId, responseHex);
-                            if (blockchainVerification) {
-                                return {
-                                    finalized: blockchainVerification,
-                                    result: {
-                                        stateConnectorRound: matchedResponse.stateConnectorRound,
-                                        merkleProof: proof ? proof : undefined,
-                                        blockNumber: toBN(matchedResponse.blockNumber),
-                                        blockTimestamp: toBN(matchedResponse.blockTimestamp),
-                                        numberOfConfirmations: toBN(matchedResponse.numberOfConfirmations),
-                                        averageBlockProductionTimeMs: toBN(matchedResponse.averageBlockProductionTimeMs),
-                                        lowestQueryWindowBlockNumber: toBN(matchedResponse.lowestQueryWindowBlockNumber),
-                                        lowestQueryWindowBlockTimestamp: toBN(matchedResponse.lowestQueryWindowBlockTimestamp),
-                                    }
-                                }
-                            }
-                            break;
-                        case AttestationType.ReferencedPaymentNonexistence:
-                            console.log('verifyReferencedPaymentNonexistence');
-                            blockchainVerification = await attestationClient.verifyReferencedPaymentNonexistence(sourceId, responseHex);
-                            if (blockchainVerification) {
-                                return {
-                                    finalized: blockchainVerification,
-                                    result: {
-                                        stateConnectorRound: matchedResponse.stateConnectorRound,
-                                        merkleProof: proof ? proof : undefined,
-                                        deadlineBlockNumber: toBN(matchedResponse.deadlineBlockNumber),
-                                        deadlineTimestamp: toBN(matchedResponse.deadlineTimestamp),
-                                        destinationAddressHash: matchedResponse.destinationAddressHash,
-                                        paymentReference: matchedResponse.paymentReference,
-                                        amount: toBN(matchedResponse.amount),
-                                        lowerBoundaryBlockNumber: toBN(matchedResponse.lowerBoundaryBlockNumber),
-                                        lowerBoundaryBlockTimestamp: toBN(matchedResponse.lowerBoundaryBlockTimestamp),
-                                        firstOverflowBlockNumber: toBN(matchedResponse.firstOverflowBlockNumber),
-                                        firstOverflowBlockTimestamp: toBN(matchedResponse.firstOverflowBlockTimestamp),
-                                    }
-                                }
-                            }
-                            break;
-                        default:
-                            break;
-                    }
-                } catch (e) {
-                    console.log(e);
-                }
+            if (matchedResponse == null) {
+                // round is finalized, but this request hasn't been proved (it is false)
+                return { finalized: true, result: null };
             }
+
+            // build Merkle tree, obtain proof, and check root
+            const hashes: string[] = data.map((item: any) => item.hash) as string[];
+            const tree = new MerkleTree(hashes);
+            const index = tree.sortedHashes.findIndex((hash) => hash === matchedResponse.hash);
+            const proof = tree.getProof(index);
+            if (proof == null) {
+                // this should never happen, unless there is bug in the MerkleTree implementation
+                throw new StateConnectorError(`Cannot obtain Merkle proof`);
+            }
+
+            // gets the root and checks that it is available (throws if it is not)
+            const scFinalizedRoot = await this.stateConnector.merkleRoot(round);
+            if (scFinalizedRoot !== tree.root) {
+                // this can only happen if the attestation provider from where we picked data is
+                // inconsistent with the finalized Merkle root in the blockchain
+                throw new StateConnectorError(`SC Merkle roots mismatch ${scFinalizedRoot} != ${tree.root}`);
+            }
+
+            // convert the proof
+            const proofData = this.decodeProof(matchedResponse, proof);
+            
+            // extra verification - should never fail, since Merkle root matches
+            const verified = this.verifyProof(matchedResponse.request.sourceId, matchedResponse.request.attestationType, proofData);
+            if (!verified) {
+                // console.error(JSON.stringify(web3DeepNormalize(proofData)));
+                throw new StateConnectorError("Proof does not verify!!!")
+            }
+            
+            return { finalized: true, result: proofData };
+        } catch (e) {
+            if (e instanceof StateConnectorError) throw e;
+            throw new StateConnectorError(String(e));
         }
-        return {
-            finalized: false,
-            result: null
+    }
+    
+    private async verifyProof(sourceId: BNish, type: AttestationType, proofData: DHType) {
+        const normalizedProofData = web3DeepNormalize(proofData);
+        switch (type) {
+            case AttestationType.Payment:
+                return await this.attestationClient.verifyPayment(sourceId, normalizedProofData as any);
+            case AttestationType.BalanceDecreasingTransaction:
+                return await this.attestationClient.verifyBalanceDecreasingTransaction(sourceId, normalizedProofData as any);
+            case AttestationType.ConfirmedBlockHeightExists:
+                return await this.attestationClient.verifyConfirmedBlockHeightExists(sourceId, normalizedProofData as any);
+            case AttestationType.ReferencedPaymentNonexistence:
+                return await this.attestationClient.verifyReferencedPaymentNonexistence(sourceId, normalizedProofData as any);
+            default:
+                throw new StateConnectorError(`Invalid attestation type ${type}`);
         }
     }
 
+    private decodeProof(matchedResponse: any, proof: string[]): DHType {
+        switch (matchedResponse.request.attestationType) {
+            case AttestationType.Payment:
+                console.log('Payment');
+                return this.decodePayment(matchedResponse, proof);
+            case AttestationType.BalanceDecreasingTransaction:
+                console.log('verifyBalanceDecreasingTransaction');
+                return this.decodeBalanceDecreasingTransaction(matchedResponse, proof);
+            case AttestationType.ConfirmedBlockHeightExists:
+                console.log('verifyConfirmedBlockHeightExists');
+                return this.decodeConfirmedBlockHeightExists(matchedResponse, proof);
+            case AttestationType.ReferencedPaymentNonexistence:
+                console.log('verifyReferencedPaymentNonexistence');
+                return this.decodeReferencedPaymentNonexistence(matchedResponse, proof);
+            default:
+                throw new StateConnectorError(`Invalid attestation type ${matchedResponse.request.attestationType}`);
+        }
+    }
+
+    private decodePayment(matchedResponse: any, proof: string[]): DHPayment {
+        return {
+            stateConnectorRound: matchedResponse.stateConnectorRound,
+            merkleProof: proof,
+            blockNumber: toBN(matchedResponse.blockNumber),
+            blockTimestamp: toBN(matchedResponse.blockTimestamp),
+            transactionHash: matchedResponse.transactionHash,
+            inUtxo: toBN(matchedResponse.inUtxo),
+            utxo: toBN(matchedResponse.utxo),
+            sourceAddressHash: matchedResponse.sourceAddressHash,
+            receivingAddressHash: matchedResponse.receivingAddressHash,
+            spentAmount: toBN(matchedResponse.spentAmount),
+            receivedAmount: toBN(matchedResponse.receivedAmount),
+            paymentReference: matchedResponse.paymentReference,
+            oneToOne: matchedResponse.oneToOne,
+            status: toBN(matchedResponse.status)
+        };
+    }
+
+    private decodeBalanceDecreasingTransaction(matchedResponse: any, proof: string[]): DHBalanceDecreasingTransaction {
+        return {
+            stateConnectorRound: matchedResponse.stateConnectorRound,
+            merkleProof: proof,
+            blockNumber: toBN(matchedResponse.blockNumber),
+            blockTimestamp: toBN(matchedResponse.blockTimestamp),
+            transactionHash: matchedResponse.transactionHash,
+            inUtxo: toBN(matchedResponse.inUtxo),
+            sourceAddressHash: matchedResponse.sourceAddressHash,
+            spentAmount: toBN(matchedResponse.spentAmount),
+            paymentReference: matchedResponse.paymentReference
+        };
+    }
+
+    private decodeConfirmedBlockHeightExists(matchedResponse: any, proof: string[]): DHConfirmedBlockHeightExists {
+        return {
+            stateConnectorRound: matchedResponse.stateConnectorRound,
+            merkleProof: proof,
+            blockNumber: toBN(matchedResponse.blockNumber),
+            blockTimestamp: toBN(matchedResponse.blockTimestamp),
+            numberOfConfirmations: toBN(matchedResponse.numberOfConfirmations),
+            averageBlockProductionTimeMs: toBN(matchedResponse.averageBlockProductionTimeMs),
+            lowestQueryWindowBlockNumber: toBN(matchedResponse.lowestQueryWindowBlockNumber),
+            lowestQueryWindowBlockTimestamp: toBN(matchedResponse.lowestQueryWindowBlockTimestamp),
+        };
+    }
+
+    private decodeReferencedPaymentNonexistence(matchedResponse: any, proof: string[]): DHReferencedPaymentNonexistence {
+        return {
+            stateConnectorRound: matchedResponse.stateConnectorRound,
+            merkleProof: proof,
+            deadlineBlockNumber: toBN(matchedResponse.deadlineBlockNumber),
+            deadlineTimestamp: toBN(matchedResponse.deadlineTimestamp),
+            destinationAddressHash: matchedResponse.destinationAddressHash,
+            paymentReference: matchedResponse.paymentReference,
+            amount: toBN(matchedResponse.amount),
+            lowerBoundaryBlockNumber: toBN(matchedResponse.lowerBoundaryBlockNumber),
+            lowerBoundaryBlockTimestamp: toBN(matchedResponse.lowerBoundaryBlockTimestamp),
+            firstOverflowBlockNumber: toBN(matchedResponse.firstOverflowBlockNumber),
+            firstOverflowBlockTimestamp: toBN(matchedResponse.firstOverflowBlockTimestamp),
+        };
+    }
 }
