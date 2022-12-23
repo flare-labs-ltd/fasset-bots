a258f431137dbf30af444d39dff496c1fd3223f4
Urska Krivc <urska.krivc>
2022-12-23 10:24:16 +0100

    small change

--- test-hardhat/utils/helpers.ts
+++ test-hardhat/utils/helpers.ts
@@ -1,4 +1,4 @@
-import { TraceManager } from "@flarenetwork/mcc/dist/src/utils/trace";
+import { TraceManager } from "@flarenetwork/mcc";
 import { TraceManager as TraceManagerSimpleWallet } from "../../node_modules/simple-wallet/node_modules/@flarenetwork/mcc/dist/src/utils/trace";
 
 export function disableMccTraceManager() {
