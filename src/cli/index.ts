739627134db59185a7cf6e3c60109042512bdbfc
Urska Krivc <urska.krivc>
2023-01-30 10:41:47 +0100

    cli changes

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { createAgentVault, depositToVault, enterAvailableList, exitAvailableList } from "./commands";
 
 const { Command } = require("commander");
@@ -6,20 +8,30 @@ const figlet = require("figlet");
 const program = new Command();
 console.log(figlet.textSync("FAsset bots"));
 
-program.command('create <ownerAddress>')
+program
+  .name("fassetBotsCli")
+
+program.command("create")
     .description("create new agent vault from owner's address")
+    .argument("<ownerAddress>", "owner's address")
     .action(createAgentVault);
 
-program.command('deposit <amount> <agentVault>')
+program.command("deposit")
     .description("deposit amount to agent vault from owner's address")
+    .argument("<amount>", "amount to deposit")
+    .argument("<agentVault>", "agent's vault address")
     .action(depositToVault);
 
-program.command('enter <agentVault> <feeBIPS> <collateralRatioBIPS>')
+program.command("enter")
     .description("enter available agent's list")
+    .argument("<agentVault>", "agent's vault address")
+    .argument("<feeBIPS>", "fee charged to minters (in underlying currency)")
+    .argument("<agentMinCollateralRatioBIPS>", "ratio at which free collateral for the minting will be accounted")
     .action(enterAvailableList);
 
-program.command('exit <agentVault>')
+program.command("exit")
     .description("exit available agent's list")
+    .argument("<agentVault>", "agent's vault address")
     .action(exitAvailableList);
 
 program.parse();
