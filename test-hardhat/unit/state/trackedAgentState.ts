62a16a458f51692395f89b6de920656aa639cb53
Urska Krivc <urska.krivc>
2023-03-01 10:52:50 +0100

    remove unused

--- test-hardhat/unit/state/trackedAgentState.ts
+++ test-hardhat/unit/state/trackedAgentState.ts
@@ -4,11 +4,9 @@ import { EM, ORM } from "../../../src/config/orm";
 import { IAssetBotContext } from "../../../src/fasset-bots/IAssetBotContext";
 import { AgentInfo, AssetManagerSettings } from "../../../src/fasset/AssetManagerTypes";
 import { overrideAndCreateOrm } from "../../../src/mikro-orm.config";
-import { MockChain } from "../../../src/mock/MockChain";
-import { Prices } from "../../../src/state/Prices";
 import { TrackedAgentState } from "../../../src/state/TrackedAgentState";
 import { TrackedState } from "../../../src/state/TrackedState";
-import { checkedCast, MAX_UINT256, toBN, toBNExp } from "../../../src/utils/helpers";
+import { MAX_UINT256, toBN, toBNExp } from "../../../src/utils/helpers";
 import { web3 } from "../../../src/utils/web3";
 import { createTestOrmOptions } from "../../../test/test-utils/test-bot-config";
 import { testChainInfo } from "../../../test/test-utils/TestChainInfo";
@@ -19,24 +17,16 @@ describe("Tracked agent state tests", async () => {
     let context: IAssetBotContext; // due to ftsoManagerMock's mockFinalizePriceEpoch()
     let orm: ORM;
     let ownerAddress: string;
-    let chain: MockChain;
     let agentBot: AgentBot;
     let trackedAgentState: TrackedAgentState;
     let trackedState: TrackedState;
     let agentInfo: AgentInfo;
     let settings: AssetManagerSettings;
-    let prices: Prices;
 
     async function createTestAgentBot(rootEm: EM, context: IAssetBotContext, address: string): Promise<AgentBot> {
         const agentBot = await AgentBot.create(rootEm, context, address);
         await agentBot.agent.depositCollateral(toBNExp(100_000_000, 18));
         return agentBot;
-    }
-
-    async function createPrices(context: IAssetBotContext): Promise<Prices> {
-        const { 0: natPrice, 1: natTimestamp } = await context.natFtso.getCurrentPrice();
-        const { 0: assetPrice, 1: assetTimestamp } = await context.assetFtso.getCurrentPrice();
-        return new Prices(settings, natPrice, natTimestamp, assetPrice, assetTimestamp);
     }
 
     before(async () => {
@@ -45,9 +35,7 @@ describe("Tracked agent state tests", async () => {
         orm = await overrideAndCreateOrm(createTestOrmOptions({ schemaUpdate: 'recreate' }));
         orm.em.clear();
         context = await createTestAssetContext(accounts[0], testChainInfo.xrp, false);
-        chain = checkedCast(context.chain, MockChain);
         settings = await context.assetManager.getSettings();
-        prices = await createPrices(context);
     });
 
     beforeEach(async () => {
@@ -71,4 +59,3 @@ describe("Tracked agent state tests", async () => {
     });
 
 });
-
