f117be18e2d2affb794d90a7a0b951d0117a5853
Urska Krivc <urska.krivc>
2023-04-14 10:01:45 +0200

    add TokenPrice and TokenPriceReader tests

---
+++ test-hardhat/unit/state/tokenPrice.ts
@@ -0,0 +1,72 @@
+import { TokenPrice, TokenPriceReader } from "../../../src/state/TokenPrice";
+import { toBN } from "../../../src/utils/helpers";
+import { TestAssetBotContext, createTestAssetContext } from "../../test-utils/create-test-asset-context";
+import { web3 } from "../../../src/utils/web3";
+import { testChainInfo } from "../../../test/test-utils/TestChainInfo";
+import { artifacts } from "../../../src/utils/artifacts";
+import { AssetManagerSettings } from "../../../src/fasset/AssetManagerTypes";
+import chaiAsPromised from "chai-as-promised";
+import { expect, use } from "chai";
+use(chaiAsPromised);
+
+const IFtsoRegistry = artifacts.require("IFtsoRegistry");
+
+describe("Token price unit tests", async () => {
+    let context: TestAssetBotContext;
+
+    before(async () => {
+        const accounts = await web3.eth.getAccounts();
+        context = await createTestAssetContext(accounts[0], testChainInfo.xrp);
+    });
+
+    it("Should create TokenPrice", async () => {
+        const price = toBN(123456);
+        const timestamp = toBN(123);
+        const decimals = toBN(6);
+        const tokenPrice = new TokenPrice(price, timestamp, decimals);
+        expect(tokenPrice).to.not.be.null;
+        expect(tokenPrice.toNumber()).eq(0.123456);
+        expect(tokenPrice.toFixed()).eq("0.123");
+        expect(tokenPrice.toString()).eq("0.123");
+        expect(tokenPrice.amgToTokenWei(await context.assetManager.getSettings(), tokenPrice.decimals, tokenPrice).toString()).eq("1000000000");
+    });
+
+});
+
+describe("Token price reader unit tests", async () => {
+    let settings: AssetManagerSettings;
+
+    before(async () => {
+        const accounts = await web3.eth.getAccounts();
+        const context = await createTestAssetContext(accounts[0], testChainInfo.xrp);
+        settings = await context.assetManager.getSettings();
+    });
+
+    it("Should create TokenPriceReader", async () => {
+        const ftsoRegistry = await IFtsoRegistry.at(settings.ftsoRegistry);
+        const priceReader = new TokenPriceReader(ftsoRegistry);
+        expect(priceReader).to.not.be.null;
+    });
+
+    it("Should return Ftso", async () => {
+        const ftsoRegistry = await IFtsoRegistry.at(settings.ftsoRegistry);
+        const priceReader = new TokenPriceReader(ftsoRegistry);
+        const ftsoSymbol = "NAT";
+        const ftso = await priceReader.getFtso(ftsoSymbol);
+        expect(await ftso.symbol()).to.eq(ftsoSymbol);
+        await expect(priceReader.getFtso("NOT")).to.eventually.be.rejected.and.be.an.instanceOf(Error);
+    });
+
+    it("Should return price", async () => {
+        const ftsoRegistry = await IFtsoRegistry.at(settings.ftsoRegistry);
+        const priceReader = new TokenPriceReader(ftsoRegistry);
+        const ftsoSymbol = "NAT";
+        const price0 = await priceReader.getPrice(ftsoSymbol);
+        const price1 = await priceReader.getPrice(ftsoSymbol, false, toBN(0));
+        const price2 = await priceReader.getPrice(ftsoSymbol, true, toBN(0));
+        expect(price0.toString()).to.not.be.null;
+        expect(price1.toString()).to.not.be.null;
+        expect(price2.toString()).to.not.be.null;
+    });
+
+});
