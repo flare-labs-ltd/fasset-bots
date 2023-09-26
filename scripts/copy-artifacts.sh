9582c4512401e40f274d3125c63cfca66ea50d99
Iztok <iztok@abelium.com>
2023-09-26 09:09:20 +0200

    renamed artifact paths to match source name field

--- scripts/copy-artifacts.sh
+++ scripts/copy-artifacts.sh
@@ -23,3 +23,8 @@ find -name '*.json' -not -name '*.dbg.json' -not -path './build-info/*' -not -pa
 cd ${fassetsdir}
 cp -R typechain-truffle ${projdir}
 cp ${projdir}/scripts/types.d.ts ${projdir}/typechain-truffle
+
+# fix some paths so that sourceName always matches the actual dir
+cd ${projdir}
+mkdir -p artifacts/flattened
+mv artifacts/flare-sc artifacts/flattened/FlareSmartContracts.sol
