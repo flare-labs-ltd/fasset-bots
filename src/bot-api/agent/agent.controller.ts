b68150dc1152a19752d200930daa0b408c0ae107
Urska Krivc <urska.krivc>
2023-09-28 13:02:16 +0200

    add agent create api

--- src/bot-api/agent/agent.controller.ts
+++ src/bot-api/agent/agent.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AgentService } from './agent.service';
 import { ApiTags } from "@nestjs/swagger";
+import { ApiResponseWrapper, handleApiResponse } from '../common/ApiResponse';
+import { AgentCreateResponse } from '../common/AgentResponse';
 
 @ApiTags("Agent")
 @Controller("api/agent")
 export class AgentController {
-  constructor(private readonly appService: AgentService) {}
+  constructor(private readonly agentService: AgentService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("create/:fAssetSymbol")
+  public async create(@Param("fAssetSymbol") fAssetSymbol: string): Promise<ApiResponseWrapper<AgentCreateResponse | null>> {
+    return handleApiResponse(this.agentService.createAgent(fAssetSymbol));
   }
 }
+
 
 
 
--- src/bot-api/agent/agent.service.ts
+++ src/bot-api/agent/agent.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { BotCliCommands } from '../../actors/AgentBotCliCommands';
+import { AgentCreateResponse } from '../common/AgentResponse';
 
 @Injectable()
 export class AgentService {
-  getHello(): string {
-    return 'Hello World!';
+
+  async createAgent(fAssetSymbol: string): Promise<AgentCreateResponse | null> {
+    const cli = await BotCliCommands.create(fAssetSymbol);
+    const agent = await cli.createAgentVault();
+    if (agent) {
+      return {
+        vaultAddress: agent.vaultAddress,
+        ownerAddress: agent.ownerAddress,
+        collateralPoolAddress: agent.collateralPool.address,
+        collateralPoolTokenAddress: agent.collateralPoolToken.address,
+        underlyingAddress: agent.underlyingAddress
+      }
+    } return null;
   }
 }
