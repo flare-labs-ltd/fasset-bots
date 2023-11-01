8ee19e13ef09b97b30cb3c61635953af73793ad9
JeznaSpianca <jurij.kolenik@gmail.com>
2023-11-01 18:19:55 +0100

    Agent destroy fix

--- src/actors/AgentBot.ts
+++ src/actors/AgentBot.ts
@@ -631,33 +631,23 @@ export class AgentBot {
             } else if (agentEnt.waitingForDestructionCleanUp) {
                 logger.info(`Agent ${this.agent.vaultAddress} is checking if clean up before destruction is complete.`);
                 // agent checks if clean up is complete
-                // withdraw pool fees
+                // withdraw and self close pool fees
                 const poolFeeBalance = await this.agent.poolFeeBalance();
                 if (poolFeeBalance.gt(BN_ZERO)) {
                     await this.agent.withdrawPoolFees(poolFeeBalance);
-                    logger.info(`Agent ${this.agent.vaultAddress} withdrew pool fees ${poolFeeBalance.toString()}.`);
+                    await this.agent.selfClose(poolFeeBalance);
+                    logger.info(`Agent ${this.agent.vaultAddress} withdrew and self closed pool fees ${poolFeeBalance.toString()}.`);
                 }
-                // check poolTokens and vaultCollateralBalance
-                const agentInfoForAnnounce = await this.agent.getAgentInfo();
-                const freeVaultCollateralBalance = toBN(agentInfoForAnnounce.freeVaultCollateralWei);
-                const freePoolTokenBalance = toBN(agentInfoForAnnounce.freePoolCollateralNATWei);
-                if (freeVaultCollateralBalance.gt(BN_ZERO)) {
-                    // announce withdraw class 1
-                    agentEnt.destroyVaultCollateralWithdrawalAllowedAtTimestamp =
-                        await this.agent.announceVaultCollateralWithdrawal(freeVaultCollateralBalance);
-                    agentEnt.destroyVaultCollateralWithdrawalAllowedAtAmount = freeVaultCollateralBalance.toString();
-                    logger.info(
-                        `Agent ${this.agent.vaultAddress} announced vault collateral withdrawal ${freeVaultCollateralBalance.toString()} at ${
-                            agentEnt.destroyVaultCollateralWithdrawalAllowedAtTimestamp
-                        }.`
-                    );
-                }
-                if (freePoolTokenBalance.gt(BN_ZERO)) {
+                // check poolTokens
+                const poolTokenBalance = toBN(await this.agent.collateralPoolToken.balanceOf(this.agent.vaultAddress));
+                const totalPoolTokens = toBN(await this.agent.collateralPoolToken.totalSupply());
+                if (poolTokenBalance.gt(BN_ZERO)) {
                     // announce redeem pool tokens and wait for others to do so (pool needs to be empty)
-                    agentEnt.poolTokenRedemptionWithdrawalAllowedAtTimestamp = await this.agent.announcePoolTokenRedemption(freePoolTokenBalance);
-                    agentEnt.poolTokenRedemptionWithdrawalAllowedAtAmount = freePoolTokenBalance.toString();
+                    console.log(poolTokenBalance);
+                    agentEnt.poolTokenRedemptionWithdrawalAllowedAtTimestamp = await this.agent.announcePoolTokenRedemption(poolTokenBalance);
+                    agentEnt.poolTokenRedemptionWithdrawalAllowedAtAmount = poolTokenBalance.toString();
                     logger.info(
-                        `Agent ${this.agent.vaultAddress} announced pool token redemption ${freePoolTokenBalance.toString()} at ${
+                        `Agent ${this.agent.vaultAddress} announced pool token redemption ${poolTokenBalance.toString()} at ${
                             agentEnt.poolTokenRedemptionWithdrawalAllowedAtTimestamp
                         }.`
                     );
@@ -665,8 +655,8 @@ export class AgentBot {
                 const agentInfoForDestroy = await this.agent.getAgentInfo();
                 //and wait for others to redeem
                 if (
-                    freePoolTokenBalance.eq(BN_ZERO) &&
-                    freeVaultCollateralBalance.eq(BN_ZERO) &&
+                    poolTokenBalance.eq(BN_ZERO) &&
+                    totalPoolTokens.eq(BN_ZERO) &&
                     toBN(agentInfoForDestroy.mintedUBA).eq(BN_ZERO) &&
                     toBN(agentInfoForDestroy.redeemingUBA).eq(BN_ZERO) &&
                     toBN(agentInfoForDestroy.reservedUBA).eq(BN_ZERO) &&
