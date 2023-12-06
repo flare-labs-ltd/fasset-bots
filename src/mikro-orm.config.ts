1b20b10126073e8003830e0dc75d5b2ceecda751
kuco23 <nseverkar@gmail.com>
2023-12-06 08:20:44 +0100

    mikroorm secrets require fix

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -4,7 +4,7 @@ import { AgentEntity, AgentMinting, AgentRedemption } from "./entities/agent";
 import { WalletAddress } from "./entities/wallet";
 import { createOrm, CreateOrmOptions, ORM } from "./config/orm";
 import { PostgreSqlDriver } from "@mikro-orm/postgresql";
-import { getSecrets } from "./config/secrets";
+import { Secrets, getSecrets } from "./config/secrets";
 
 /* istanbul ignore next */
 const options: Options<AbstractSqlDriver | PostgreSqlDriver> = {
@@ -14,9 +14,10 @@ const options: Options<AbstractSqlDriver | PostgreSqlDriver> = {
 };
 
 export async function overrideAndCreateOrm(optionsOverride: CreateOrmOptions): Promise<ORM> {
-    const secrets = getSecrets();
+    let secrets = { database: {} } as Secrets;
+    try { secrets = getSecrets(); } catch (e) {}
     const createOptions: CreateOrmOptions = { ...options, ...secrets.database, ...optionsOverride };
-    return await createOrm(createOptions);
+    return createOrm(createOptions);
 }
 
 export default options;
