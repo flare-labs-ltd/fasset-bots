31c039a4df53bd4f66a65b3b24b001820dfe0d78
Iztok <iztok@abelium.com>
2023-12-20 11:52:46 +0100

    moved files to packages

---
+++ packages/core-lib/src/index.ts
@@ -0,0 +1,6 @@
+export { ActorBaseRunner } from "./actors/ActorBaseRunner";
+export { BotCliCommands } from "./actors/AgentBotCliCommands";
+export { AgentBotRunner } from "./actors/AgentBotRunner";
+export { TimeKeeper } from "./actors/TimeKeeper";
+export { UserBot } from "./actors/UserBot";
+export { ActorBaseKind } from "./fasset-bots/ActorBase";
