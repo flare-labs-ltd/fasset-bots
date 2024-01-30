b4195cbb909bf5fa95902f169460de32389d3517
Iztok <iztok@abelium.com>
2024-01-30 18:21:00 +0100

    fixed agent settings schema

--- packages/fasset-bots-core/src/config/config-files.ts
+++ packages/fasset-bots-core/src/config/config-files.ts
@@ -76,50 +76,50 @@ export interface AgentSettingsConfig {
 
     /**
      * The minting fee percentage.
-     * @pattern ^\d+(\.\d+)?\%?$
+     * @pattern ^\d+(\.\d+)?%?$
      */
     fee: string;
 
     /**
      * The percentage of the minting fee that goes to the collateral pool.
-     * @pattern ^\d+(\.\d+)?\%?$
+     * @pattern ^\d+(\.\d+)?%?$
      */
     poolFeeShare: string;
 
     /**
      * Agent's minting collateral ratio for vault collateral (minimum CR at which the minting can happen).
-     * @pattern ^\d+(\.\d+)?\%?$
+     * @pattern ^\d+(\.\d+)?%?$
      */
     mintingVaultCollateralRatio: string;
 
     /**
      * Agent's minting collateral ratio for pool collateral (minimum CR at which the minting can happen).
-     * @pattern ^\d+(\.\d+)?\%?$
+     * @pattern ^\d+(\.\d+)?%?$
      */
     mintingPoolCollateralRatio: string;
 
     /**
      * Collateral pool's exit collateral ratio (minimum CR for pool collateral at which the collateral pool providers can exit;
      * however, self-close exit is allowed even at lower pool CR).
-     * @pattern ^\d+(\.\d+)?\%?$
+     * @pattern ^\d+(\.\d+)?%?$
      */
     poolExitCollateralRatio: string;
 
     /**
      * FTSO price factor at which the agent pays for burned fassets (in vault tokens) during pool providers' self close exit.
-     * @pattern ^\d+(\.\d+)?\%?$
+     * @pattern ^\d+(\.\d+)?%?$
      */
     buyFAssetByAgentFactor: string;
 
     /**
      * Pool collateral ratio below which the providers can enter at discounted rate.
-     * @pattern ^\d+(\.\d+)?\%?$
+     * @pattern ^\d+(\.\d+)?%?$
      */
     poolTopupCollateralRatio: string;
 
     /**
      * Discounted price factor at which providers can enter when topup is active (i.e. the pool CR is below poolTopupCollateralRatio).
-     * @pattern ^\d+(\.\d+)?\%?$
+     * @pattern ^\d+(\.\d+)?%?$
      */
     poolTopupTokenPriceFactor: string;
 }
