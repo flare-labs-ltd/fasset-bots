cd91408bbe876f02541533649bca53b974504998
Urška Krivc <urska.krivc@abelium.com>
2024-05-23 14:24:57 +0200

    reuse fn fetchAgentEntity in updateAgentEntity

--- packages/fasset-bots-core/src/actors/AgentBot.ts
+++ packages/fasset-bots-core/src/actors/AgentBot.ts
@@ -788,7 +788,7 @@ export class AgentBot {
      */
     async updateAgentEntity(rootEm: EM, modify: (agentEnt: AgentEntity) => Promise<void>): Promise<void> {
         await rootEm.transactional(async (em) => {
-            const agentEnt: AgentEntity = await rootEm.findOneOrFail(AgentEntity, { vaultAddress: this.agent.vaultAddress } as FilterQuery<AgentEntity>, { refresh: true });
+            const agentEnt: AgentEntity = await this.fetchAgentEntity(rootEm);
             await modify(agentEnt);
             await em.persistAndFlush(agentEnt);
         });
