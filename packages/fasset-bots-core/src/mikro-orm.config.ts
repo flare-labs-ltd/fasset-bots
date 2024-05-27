ad87daaac96b6688833f623d1181dcf2f6459a59
Urška Krivc <urska.krivc@abelium.com>
2024-05-27 10:17:50 +0200

    add AgentUnderlyingPayment entity

--- packages/fasset-bots-core/src/mikro-orm.config.ts
+++ packages/fasset-bots-core/src/mikro-orm.config.ts
@@ -2,12 +2,12 @@ import { Options } from "@mikro-orm/core";
 import { AbstractSqlDriver } from "@mikro-orm/knex";
 import { DatabaseAccount } from "./config/config-files/SecretsFile";
 import { CreateOrmOptions, ORM, createOrm } from "./config/orm";
-import { AgentEntity, AgentMinting, AgentRedemption, Event } from "./entities/agent";
+import { AgentEntity, AgentMinting, AgentRedemption, AgentUnderlyingPayment, Event } from "./entities/agent";
 import { WalletAddress } from "./entities/wallet";
 
 /* istanbul ignore next */
 const options: Options<AbstractSqlDriver> = {
-    entities: [WalletAddress, AgentEntity, AgentMinting, AgentRedemption, Event],
+    entities: [WalletAddress, AgentEntity, AgentMinting, AgentRedemption, Event, AgentUnderlyingPayment],
     dbName: "fasset-bots.db",
     debug: false
 };
