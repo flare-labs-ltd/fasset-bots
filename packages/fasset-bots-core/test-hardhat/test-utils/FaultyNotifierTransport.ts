a94d5fdb296526daee06f51af7967c13224ec590
Iztok <iztok@abelium.com>
2024-06-14 10:31:03 +0200

    test hardhat upgrade

--- packages/fasset-bots-core/test-hardhat/test-utils/FaultyNotifierTransport.ts
+++ packages/fasset-bots-core/test-hardhat/test-utils/FaultyNotifierTransport.ts
@@ -1,5 +1,7 @@
 import MockAdapter from "axios-mock-adapter";
 import { ApiNotifierTransport } from "../../src/utils/notifier/NotifierTransports";
+import { BotType, NotificationLevel } from "../../src/utils/notifier/BaseNotifier";
+import { sleep } from "../../src/utils";
 
 // to use in tests
 export class FaultyNotifierTransport extends ApiNotifierTransport {
@@ -7,8 +9,12 @@ export class FaultyNotifierTransport extends ApiNotifierTransport {
 
     constructor() {
         super("FaultyNotifier", "MockApiKey");
-        if (this.mock) {
-            this.mock.onPost("/api/0/bot_alert").reply(500, "Internal Server Error");
-        }
+        this.mock = new MockAdapter(this.client);
+        this.mock.onPost("/api/0/bot_alert").reply(500, "Internal Server Error");
+    }
+
+    override async send(type: BotType, address: string, level: NotificationLevel, title: string, message: string): Promise<void> {
+        await super.send(type, address, level, title, message);
+        await sleep(1000);
     }
 }
