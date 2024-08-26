7384a01976c228bc10b078b2b9492619ac69b1bb
Urška Krivc <urska.krivc@gmail.com>
2024-08-26 07:25:23 +0200

    add createdAt and updatedAt in db entities

--- packages/simple-wallet/src/entity/wallet.ts
+++ packages/simple-wallet/src/entity/wallet.ts
@@ -10,4 +10,10 @@ export class WalletAddressEntity {
 
     @Property()
     isDeleting: boolean = false;
+
+    @Property({ onCreate: () => new Date() })
+    createdAt: Date = new Date();
+
+    @Property({ onUpdate: () => new Date() })
+    updatedAt: Date = new Date();
 }
