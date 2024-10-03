f30f77289a18f96ad3123d8761f84fb300f0281d
Urška Krivc <urska.krivc@gmail.com>
2024-10-03 10:15:51 +0200

    small change in loggs

--- packages/simple-wallet/src/chain-clients/implementations/XrpWalletImplementation.ts
+++ packages/simple-wallet/src/chain-clients/implementations/XrpWalletImplementation.ts
@@ -74,7 +74,7 @@ export class XrpWalletImplementation extends XrpAccountGeneration implements Wri
    async getAccountBalance(account: string): Promise<BN> {
       try {
          const data = await this.getAccountInfo(account);
-         logger.info(`Get account balance for ${account}`, data.result);
+         logger.info(`Get account balance for ${account}`, data.result.account_data?.Balance || 0);
          return toBN(data.result.account_data?.Balance || 0);
       } catch (error) {
          logger.error(`Cannot get account balance for ${account}`, error);
