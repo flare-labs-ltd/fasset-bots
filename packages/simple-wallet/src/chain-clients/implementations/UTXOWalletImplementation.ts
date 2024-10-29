8e52052fc82fdb14e4435aeab80107793751e4d8
Urška Krivc <urska.krivc@abelium.com>
2024-10-29 14:19:13 +0100

    fix for FAS0-023

--- packages/simple-wallet/src/chain-clients/implementations/UTXOWalletImplementation.ts
+++ packages/simple-wallet/src/chain-clients/implementations/UTXOWalletImplementation.ts
@@ -445,14 +445,14 @@ export abstract class UTXOWalletImplementation extends UTXOAccountGeneration imp
     }
 
     async handleNotFound(txEnt: TransactionEntity): Promise<void> {
-            if (txEnt.status === TransactionStatus.TX_REPLACED_PENDING) { // original is not found => it was replaced by rbf
+            if (txEnt.status === TransactionStatus.TX_REPLACED_PENDING) {
                 await updateTransactionEntity(this.rootEm, txEnt.id, (txEntToUpdate) => {
                     txEntToUpdate.status = TransactionStatus.TX_REPLACED;
                     txEntToUpdate.reachedFinalStatusInTimestamp = toBN(getCurrentTimestampInSeconds());
                 });
                 logger.info(`checkSubmittedTransaction (rbf) transaction ${txEnt.id} changed status from ${TransactionStatus.TX_REPLACED_PENDING} to ${TransactionStatus.TX_CREATED}.`);
             }
-            if (txEnt.status === TransactionStatus.TX_SUBMITTED) {
+            if (txEnt.status === TransactionStatus.TX_SUBMITTED) { // TODO - legit tx 2904 - 3c5bd7395b3ee8e0c503e48044d029b760a9a0c08e9e78e66ec355b73c9a961b was marked as not found!!!
                 if (txEnt.rbfReplacementFor) { // rbf is not found => original should be accepted
                     await updateTransactionEntity(this.rootEm, txEnt.id, (txEntToUpdate) => {
                         txEntToUpdate.status = TransactionStatus.TX_FAILED;
@@ -481,11 +481,7 @@ export abstract class UTXOWalletImplementation extends UTXOAccountGeneration imp
                     });
                     logger.info(`checkSubmittedTransaction (ancestor) transaction ${txEnt.id} changed status from ${TransactionStatus.TX_SUBMITTED} to ${TransactionStatus.TX_CREATED}.`);
                 } else {
-                    await updateTransactionEntity(this.rootEm, txEnt.id, (txEntToUpdate) => {
-                        txEntToUpdate.status = TransactionStatus.TX_FAILED;
-                        txEntToUpdate.reachedFinalStatusInTimestamp = toBN(getCurrentTimestampInSeconds());
-                    });
-                    logger.info(`checkSubmittedTransaction transaction ${txEnt.id} changed status from ${TransactionStatus.TX_SUBMITTED} to ${TransactionStatus.TX_FAILED}.`);
+                    logger.warn(`checkSubmittedTransaction transaction ${txEnt.id} not found.`);
                 }
             }
     }
