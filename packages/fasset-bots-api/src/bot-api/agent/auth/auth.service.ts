f04e79928f08128c9abd1a53ac3a474581febf30
jurij <jurij.kolenik@gmail.com>
2024-10-29 12:22:33 +0100

    Fix

--- packages/fasset-bots-api/src/bot-api/agent/auth/auth.service.ts
+++ packages/fasset-bots-api/src/bot-api/agent/auth/auth.service.ts
@@ -1,17 +1,13 @@
-import { Secrets } from '@flarelabs/fasset-bots-core/config';
-import { requireEnv } from '@flarelabs/fasset-bots-core/utils';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-
-const FASSET_BOT_SECRETS: string = requireEnv("FASSET_BOT_SECRETS");
+import { cachedSecrets } from "../agentServer";
 
 @Injectable()
 export class AuthService {
     constructor(private readonly jwtService: JwtService) {}
 
     public async login(password: string) {
-        const secrets = Secrets.load(FASSET_BOT_SECRETS);
-        const validPassword = secrets.required("apiKey.agent_bot");
+        const validPassword = cachedSecrets.required("apiKey.agent_bot");
 
         if (password !== validPassword) {
           throw new UnauthorizedException('Invalid password');
