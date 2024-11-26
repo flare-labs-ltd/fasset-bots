0d9f6905b29dc1e3c8f38d17c60340d888142c39
kuco23 <nseverkar@gmail.com>
2024-11-26 12:03:58 +0100

    fix: improve logging of collateral pool token withdrawal cancelling

--- packages/fasset-bots-core/src/commands/AgentBotCommands.ts
+++ packages/fasset-bots-core/src/commands/AgentBotCommands.ts
@@ -363,7 +363,7 @@ export class AgentBotCommands {
      * Cancels agent's pool token redemption announcement.
      * @param agentVault agent's vault address
      */
-    async cancelCollateralPoolTokensAnnouncement(agentVault: string): Promise<void> {
+    async cancelCollateralPoolTokenRedemption(agentVault: string): Promise<void> {
         logger.info(`Agent ${agentVault} is trying to cancel collateral pool redemption announcement.`);
         const { agentBot } = await this.getAgentBot(agentVault);
         await agentBot.agent.announceVaultCollateralWithdrawal(BN_ZERO);
@@ -372,6 +372,7 @@ export class AgentBotCommands {
             agentEnt.poolTokenRedemptionWithdrawalAllowedAtAmount = "";
         });
         logger.info(`Agent ${agentVault} cancelled pool token redemption announcement.`);
+        console.log(`Agent ${agentVault} cancelled pool token redemption announcement.`);
     }
 
     /**
