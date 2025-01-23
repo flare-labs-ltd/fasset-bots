c55b2ba803d52f94a0adc43b63c0cf4481b68aea
Iztok <iztok@abelium.com>
2025-01-23 14:56:12 +0000

    fixed secrets tests

--- packages/fasset-bots-core/test-hardhat/unit/config/secrets.ts
+++ packages/fasset-bots-core/test-hardhat/unit/config/secrets.ts
@@ -1,7 +1,12 @@
-import { expect } from "chai";
-import { ChainId } from "../../../src";
+import fs from "node:fs";
+import { expect, use } from "chai";
+import { ChainId, generateSecrets } from "../../../src";
 import { ENCRYPTION_PASSWORD_MIN_LENGTH, Secrets } from "../../../src/config/secrets";
-import { requireEnv } from "../../../src/utils/helpers";
+import path from "node:path";
+import { FASSET_BOT_CONFIG } from "../../../test/test-utils/test-bot-config";
+import { sleep, ZERO_ADDRESS } from "../../../src/utils";
+import chaiAsPromised from "chai-as-promised";
+use(chaiAsPromised);
 
 const chainId = ChainId.testXRP;
 
@@ -56,8 +61,49 @@ describe("Secrets unit tests", () => {
         expect(fn4).to.throw(`'wallet.encryption_password' should be at least ${ENCRYPTION_PASSWORD_MIN_LENGTH} chars long`);
     });
 
-    it("Should load secrets", async () => {
-        const secrets2 = await Secrets.load("./test-hardhat/test-utils/test-secrets/test-secrets.json");
-        expect(secrets2.data.apiKey).to.not.be.empty;
+    function createSecretsFilePath(name: string) {
+        const dataPath = "./test-data";
+        if (!fs.existsSync(dataPath)) fs.mkdirSync(dataPath, { recursive: true });
+        return path.join(dataPath, name);
+    }
+
+    it("Should generate and load secrets", async () => {
+        const secretsPath = createSecretsFilePath("dummy-secrets.json");
+        const secrets = generateSecrets(FASSET_BOT_CONFIG, ["agent", "user", "other"], ZERO_ADDRESS);
+        fs.writeFileSync(secretsPath, JSON.stringify(secrets, null, 4));
+        // chmod 600 (otherwise it will fail)
+        const oldALLOW_SECRETS_ON_WINDOWS = process.env.ALLOW_SECRETS_ON_WINDOWS;
+        if (process.platform !== "win32") {
+            fs.chmodSync(secretsPath, 0o600);
+        } else if (process.env.ALLOW_SECRETS_ON_WINDOWS !== "true") {
+            process.env.ALLOW_SECRETS_ON_WINDOWS = "true";
+        }
+        // test
+        try {
+            const secrets2 = await Secrets.load(secretsPath);
+            expect(secrets2.data.apiKey).to.not.be.empty;
+        } finally {
+            process.env.ALLOW_SECRETS_ON_WINDOWS = oldALLOW_SECRETS_ON_WINDOWS;
+        }
+    });
+
+    it("Should not load secrets - not chmod 600", async () => {
+        const secretsPath = createSecretsFilePath("dummy-secrets-readable.json");
+        const secrets = generateSecrets(FASSET_BOT_CONFIG, ["agent", "user", "other"], ZERO_ADDRESS);
+        fs.writeFileSync(secretsPath, JSON.stringify(secrets, null, 4));
+        // make it fail on windows
+        const oldALLOW_SECRETS_ON_WINDOWS = process.env.ALLOW_SECRETS_ON_WINDOWS;
+        process.env.ALLOW_SECRETS_ON_WINDOWS = undefined;
+        // test
+        try {
+            const promise = Secrets.load(secretsPath);
+            if (process.platform !== "win32") {
+                await expect(promise).to.eventually.be.rejectedWith(/File .* must only be readable by the process user\. Set permission bits to 600\./);
+            } else {
+                await expect(promise).to.eventually.be.rejectedWith(/Cannot reliably check secrets file permissions on Windows\..*/);
+            }
+        } finally {
+            process.env.ALLOW_SECRETS_ON_WINDOWS = oldALLOW_SECRETS_ON_WINDOWS;
+        }
     });
 });
