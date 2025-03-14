4b242b929d717ea1924a0ad200898426e5e4eb4b
Urška Krivc <urska.krivc@abelium.com>
2025-03-14 15:05:14 +0100

    add new entities

--- packages/fasset-bots-core/src/mikro-orm.config.ts
+++ packages/fasset-bots-core/src/mikro-orm.config.ts
@@ -2,14 +2,15 @@ import { Options } from "@mikro-orm/core";
 import { AbstractSqlDriver } from "@mikro-orm/knex";
 import { DatabaseAccount } from "./config/config-files/SecretsFile";
 import { CreateOrmOptions, ORM, createOrm } from "./config/orm";
-import { AgentEntity, AgentHandshake, AgentMinting, AgentRedemption, AgentUnderlyingPayment, AgentUpdateSetting, Event, RejectedRedemptionRequest } from "./entities/agent";
+import { AgentEntity, AgentHandshake, AgentMinting, AgentRedemption, AgentUnderlyingPayment, AgentUpdateSetting, Event, RejectedRedemptionRequest, ReturnFromCoreVault, TransferToCoreVault } from "./entities/agent";
 import { HistoryItem, MonitoringStateEntity, TransactionEntity, WalletAddressEntity } from "@flarelabs/simple-wallet";
 import { ActivityTimestampEntity } from "./entities/activityTimestamp";
 
 /* istanbul ignore next */
 const options: Options<AbstractSqlDriver> = {
     entities: [WalletAddressEntity, AgentEntity, AgentMinting, AgentRedemption, Event, AgentUnderlyingPayment, AgentUpdateSetting,
-        TransactionEntity, MonitoringStateEntity, HistoryItem, ActivityTimestampEntity, AgentHandshake, RejectedRedemptionRequest],
+        TransactionEntity, MonitoringStateEntity, HistoryItem, ActivityTimestampEntity, AgentHandshake, RejectedRedemptionRequest,
+        ReturnFromCoreVault, TransferToCoreVault],
     dbName: "fasset-bots.db",
     debug: false,
 };
