fc0df81f2dbcdc77d0ad5b6d4683f9510d5c4ef1
jurij <jurij.kolenik@gmail.com>
2025-03-20 10:26:24 +0100

    Revert create asset context fix

--- packages/fasset-bots-core/src/config/create-asset-context.ts
+++ packages/fasset-bots-core/src/config/create-asset-context.ts
@@ -69,7 +69,7 @@ export async function createNativeContext(config: BotConfig, chainConfig: BotFAs
     const retriever = config.contractRetriever;
     const assetManager = chainConfig.assetManager;
     const settings = await assetManager.getSettings();
-    const coreVaultManagerAddress = chainConfig.fAssetSymbol.includes("XRP") ? await assetManager.getCoreVaultManager() : ZERO_ADDRESS;
+    const coreVaultManagerAddress = await assetManager.getCoreVaultManager();
     return {
         fAssetSymbol: chainConfig.fAssetSymbol,
         nativeChainInfo: config.nativeChainInfo,
