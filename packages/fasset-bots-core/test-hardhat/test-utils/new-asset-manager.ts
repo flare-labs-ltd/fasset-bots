c40fad202f30d2de03ff279c3326e74705fb420b
Iztok <iztok@abelium.com>
2025-03-31 16:27:17 +0200

    test parameter fixes

--- packages/fasset-bots-core/test-hardhat/test-utils/new-asset-manager.ts
+++ packages/fasset-bots-core/test-hardhat/test-utils/new-asset-manager.ts
@@ -21,6 +21,7 @@ export interface AssetManagerInitSettings extends AssetManagerSettings {
     // core vault
     coreVaultNativeAddress: string;
     coreVaultTransferFeeBIPS: BNish;
+    coreVaultTransferTimeExtensionSeconds: BNish;
     coreVaultRedemptionFeeBIPS: BNish;
     coreVaultMinimumAmountLeftBIPS: BNish;
     coreVaultMinimumRedeemLots: BNish;
@@ -80,7 +81,7 @@ export async function newAssetManager(
     await deployAndInitFacet(governanceAddress, assetManager, artifacts.require("CoreVaultFacet"), ["ICoreVault"]);
     await deployAndInitFacet(governanceAddress, assetManager, artifacts.require("CoreVaultSettingsFacet"), ["ICoreVaultSettings"],
         "initCoreVaultFacet", [ZERO_ADDRESS, assetManagerSettings.coreVaultNativeAddress,
-            assetManagerSettings.coreVaultTransferFeeBIPS, assetManagerSettings.coreVaultRedemptionFeeBIPS,
+            assetManagerSettings.coreVaultTransferFeeBIPS, assetManagerSettings.coreVaultTransferTimeExtensionSeconds, assetManagerSettings.coreVaultRedemptionFeeBIPS,
             assetManagerSettings.coreVaultMinimumAmountLeftBIPS, assetManagerSettings.coreVaultMinimumRedeemLots]);
     // verify interface implementation
     await checkAllMethodsImplemented(assetManager, interfaceSelectors);
